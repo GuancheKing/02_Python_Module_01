9e0a1ff6f75dcbc37c1a1dd86643a46f130a0496
josjimen <pepojimenezbethencourt@gmail.com>
2026-02-20 15:23:49 +0100

    ex wip

---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,9 @@
+class Plant:
+    """
+    Represents a plant with name, height and age
+    """
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+    
